6a9ece3f7832f480ea5109fd590991ba1271bac8
DJ <djwadhwa@uw.edu>
2020-02-22 23:42:50 -0800

    updating weights and biases multiple times

--- backprop.m
+++ backprop.m
@@ -9,7 +9,7 @@ n = cell(total_layers,1); %output before transfer function
 a = cell(total_layers,1); %output for neuron
 S = cell(total_layers,1); %sensitivity
 
-alpha = 0.1; %learning_rate
+alpha = 0.06; %learning_rate
 
 %feedforward
 for m = 1:total_layers
@@ -36,31 +36,34 @@ end
 
 size_of_input = size(p);
 %calculate and propagate sensitivites backwards
-for i = 1:size_of_input(1)
-    for m = total_layers:-1:1
-        diff_sig = diag((1-a{m}).*a{m});
-        if (m == total_layers)
-            S{m} = -2*diff_sig*(t(i,:)'-a{m});
-        else
-            S{m} = diff_sig*W{m+1}'*S{m+1};
-        end
-    end   
-    
-    for m = 1:total_layers
-        b{m} = b{m}-alpha*S{m};
-        
-        if (m == 1)
-            W{m} = W{m}-alpha*S{m}*p(i);
-            n{m} = W{m}*p(i,:)'+b{m};
-        elseif (m == total_layers)
-            W{m} = W{m}-alpha*S{m}*a{m-1}';
-            n{m} = W{m}*a{m-1}+b{m};
-        else
-            W{m} = W{m}-alpha*S{m}*a{m-1}';
-            n{m} = W{m}*a{m-1}+b{m};
-        end
-        a{m} = logsig(n{m}); %calculate the output for each layer
-    end
-end
 
+x = 0;
+while x < 1000
+    for i = 1:size_of_input(1)
+        for m = total_layers:-1:1
+            diff_sig = diag((1-a{m}).*a{m});
+            if (m == total_layers)
+                S{m} = -2*diff_sig*(t(i,:)'-a{m});
+            else
+                S{m} = diff_sig*W{m+1}'*S{m+1};
+            end
+        end
+        
+        for m = 1:total_layers
+            b{m} = b{m}-alpha*S{m};
+            if (m == 1)
+                W{m} = W{m}-alpha*S{m}*p(i);
+                n{m} = W{m}*p(i,:)'+b{m};
+            elseif (m == total_layers)
+                W{m} = W{m}-alpha*S{m}*a{m-1}';
+                n{m} = W{m}*a{m-1}+b{m};
+            else
+                W{m} = W{m}-alpha*S{m}*a{m-1}';
+                n{m} = W{m}*a{m-1}+b{m};
+            end
+            a{m} = logsig(n{m}); %calculate the output for each layer
+        end
+    end
+    x=x+1;
+end
 end
