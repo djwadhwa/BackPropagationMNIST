b48d31ccd4139c1727c61b9c52ea96461f361a04
DJ <djwadhwa@uw.edu>
2020-02-22 22:56:08 -0800

    added multiple input support

--- backprop.m
+++ backprop.m
@@ -1,4 +1,4 @@
-function backprop(p,t, L, s) 
+function [W,b,a] = backprop(p,t, L, s)
 % p = input, t = target, L = number of hidden layers, s = number of neurons
 
 % store all matricies
@@ -7,17 +7,20 @@ W = cell(total_layers,1); %weight
 b = cell (total_layers,1); %bias
 n = cell(total_layers,1); %output before transfer function
 a = cell(total_layers,1); %output for neuron
+S = cell(total_layers,1); %sensitivity
+
+alpha = 0.1; %learning_rate
 
 %feedforward
 for m = 1:total_layers
-    if (m == 1) 
+    if (m == 1)
         %calculate the weight, bias, n for first hidden layer
         W{m} = rand(s,length(p));
         b{m} = rand (s,1);
-        n{m} = W{m}*p'+b{m};
-    elseif (m == total_layers) 
-        % calculate the weight, bias, n for the last hidden layer  
-%       size_of_input = size(p);
+        n{m} = W{m}*p(1,:)'+b{m};
+    elseif (m == total_layers)
+        % calculate the weight, bias, n for the last hidden layer
+        %       size_of_input = size(p);
         W{m} = rand (3, s);
         b{m} = rand (3, 1);
         n{m} = W{m}*a{m-1}+b{m};
@@ -30,38 +33,34 @@ for m = 1:total_layers
     a{m} = logsig(n{m}); %calculate the output for each layer
 end
 
-S = cell(total_layers,1); 
 
+size_of_input = size(p);
 %calculate and propagate sensitivites backwards
-
-for m = total_layers:-1:1
-    diff_sig = diag((1-a{m}).*a{m});
-    if (m == total_layers)
-        S{m} = -2*diff_sig*(t-a{m});
-    else
-        S{m} = diff_sig*W{m+1}'*S{m+1};
+for i = 1:size_of_input(1)
+    for m = total_layers:-1:1
+        diff_sig = diag((1-a{m}).*a{m});
+        if (m == total_layers)
+            S{m} = -2*diff_sig*(t(i,:)'-a{m});
+        else
+            S{m} = diff_sig*W{m+1}'*S{m+1};
+        end
+    end   
+    
+    for m = 1:total_layers
+        b{m} = b{m}-alpha*S{m};
+        
+        if (m == 1)
+            W{m} = W{m}-alpha*S{m}*p(i);
+            n{m} = W{m}*p(i,:)'+b{m};
+        elseif (m == total_layers)
+            W{m} = W{m}-alpha*S{m}*a{m-1}';
+            n{m} = W{m}*a{m-1}+b{m};
+        else
+            W{m} = W{m}-alpha*S{m}*a{m-1}';
+            n{m} = W{m}*a{m-1}+b{m};
+        end
+        a{m} = logsig(n{m}); %calculate the output for each layer
     end
 end
 
-learning_rate = 0.05;
-
-for m = 1:total_layers
-    if (m == 1)
-        W{m} = W{m}-learning_rate*S{m}*p;
-    else
-        W{m} = W{m}-learning_rate*S{m}*a{m-1}';
-    end
-    b{m} = b{m}-learning_rate*S{m};
-end
-
-for m = 1:total_layers
-    if (m == 1) 
-        n{m} = W{m}*p'+b{m};
-    elseif (m == total_layers) 
-        n{m} = W{m}*a{m-1}+b{m};
-    else
-        n{m} = W{m}*a{m-1}+b{m};
-    end
-    a{m} = logsig(n{m}); %calculate the output for each layer
-end
 end
