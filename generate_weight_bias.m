b5603a9cd02c97e4f985dbaf436c45a0e5ae54f7
DJ <djwadhwa@uw.edu>
2020-02-26 10:36:47 -0800

    updated weight and bias generation and graphs

--- generate_weight_bias.m
+++ generate_weight_bias.m
@@ -1,4 +1,4 @@
 function [W,b] = generate_weight_bias(row,col)
-W = randn(row, col)*sqrt(2)/(col*row);
-b = randn(row, 1)*sqrt(2)/(col*row);
+W = randn(row, col)*sqrt(2)/(row);
+b = zeros(row, 1);
 end
