31dc64889952ab539cfd6da22950ba8c5883e33d
djwadhwa <wadhwadhananjay@gmail.com>
2020-02-25 20:17:33 -0800

    added changes to graphing function, update gitignore

--- graph_backprop.m
+++ graph_backprop.m
@@ -1,20 +1,69 @@
-function graph_backprop(p,t,test_input,test_label)
-epochs = 40;
-[W,b,mse] = backprop(p,t,2,60,0.06,epochs,10);
+function graph_backprop()
 
-plot1 = plot([1:epochs-1],mse(1:epochs-1));
-xlabel('epochs')
-ylabel('Mean Squared Error')
-title ('MSE of backprop with 10 digits')
+p = [0,1,1,1,0,1,0,0,0,1,1,0,0,0,1,1,0,0,0,1,1,0,0,0,1,0,1,1,1,0; %0
+    0,1,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0; %1
+    1,1,1,0,0,0,0,0,1,0,0,0,0,1,0,0,1,1,0,0,0,1,0,0,0,0,1,1,1,1]'; %2
 
-x = 0;
-for i = 1:length(test_input)
-    [m,ind] = max(logsig(W{2}*logsig(W{1}*test_input(:,i)+b{1})+b{2}));
-    if(test_label(i)+1 == ind)
-        x = x+1;
+% t is read as t0=[1,0,0], t1=[0,1,0], t2 = [0,0,1] by the backprop
+% alogrithm, however it is stated like this for the sake of brevity
+t = [0;1;2];
+
+% number of iterations for three digits
+epochs = 15000;
+output = 3;
+layers = 4;
+neurons = 100;
+learning_rate = 0.08;
+[W,b,mse] = backprop(p,t,layers,neurons,learning_rate,epochs,output);
+figure;
+plot1 = plot([1:epochs],mse);
+xlabel('epochs');
+ylabel('Mean Squared Error');
+name = ['MSE with ' num2str(output) ' digits, ' num2str(layers) ' layers and alpha of ' num2str(learning_rate)];
+title (name);
+
+p = loadMNISTImages("train-images.idx3-ubyte");
+t = loadMNISTLabels("train-labels.idx1-ubyte");
+
+test_inputs = loadMNISTImages("t10k-images.idx3-ubyte");
+test_labels = loadMNISTLabels("t10k-labels.idx1-ubyte");
+
+progress = zeros(4,1);
+for j = 1:4
+    if (j == 1)
+        epochs = 10;
+    elseif(j == 3)
+        epochs = 20;
+    elseif (j == 3)
+        epochs = 40;
+    else
+        epochs = 60;
     end
+    output = 10;
+    layers = 2;
+    neurons = 100;
+    learning_rate = 0.06;
+    [W,b,mse2] = backprop(p,t,layers,neurons,learning_rate,epochs,output);
+    figure;
+    plot1 = plot([1:epochs],mse2);
+    xlabel('epochs');
+    ylabel('Mean Squared Error ');
+    name = ['MSE with ' num2str(output) ' digits, ' num2str(layers) ' layers and alpha of ' num2str(learning_rate)];
+    title (name);
+    x = 0;
+    for i = 1:length(test_inputs)
+        [m,ind] = max(logsig(W{2}*logsig(W{1}*test_inputs(:,i)+b{1})+b{2}));
+        if(test_labels(i)+1 == ind)
+            x = x+1;
+        end
+    end
+    progress(j,1) = x/length(test_inputs)*100;
 end
 
-disp(x/length(test_input));
-%add_noise_input(W,b,p,t);
+figure;
+plot1 = plot([10,20,40,60],progress(:,1));
+xlabel('epochs');
+ylabel();
+title ('Test Set classification accuracy for 10, 20, 40, and 60 epcohs');
+
 end
